74cba220119c5ed5a583a6bcd1b21b0230fd016e
osmiOTALORAGUERRERO <32118923+osmiOTALORAGUERRERO@users.noreply.github.com>
2020-02-11 14:19:05 -0500

    Update files.py
    Terminando suma y creación de archivo

--- files/files.py
+++ files/files.py
@@ -1,12 +1,27 @@
-fr = open('ejemplo.txt','r')
-mensaje = fr.read().replace('\n','')
-mensaje = mensaje.split(',')
-fr.close()
+import random
+import time
 
 fw = open('ejemplo.txt','w')
 newText = ''
-for number in mensaje:
-    numberStr = str( int(number) + 1 )
-    newText += numberStr+','
+for number in range(1000):
+    newText += str(random.randrange(1000))+','
 fw.write(newText.strip(','))
+
+fw.close()
+
+fr = open('ejemplo.txt','r')
+mensaje = fr.read()
+mensaje = mensaje.split(',')
+
+second = time.time()
+suma = 0
+for num in mensaje:
+  suma += int(num)
+print(time.time() - second)
+
+
+fw = open('ejemplo1.txt','w')
+fw.write(str(suma))
+
+fr.close()
 fw.close()
