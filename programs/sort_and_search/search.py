ceda1c4bd4d64eedac540348d3ce6835de9c814f
Osmi Otalora Guerrero <ootalorague@uniminuto.edu.co>
2020-02-26 17:36:15 -0500

    algoritmos de ordenamiento y busqueda

--- programs/sort_and_search/search.py
+++ programs/sort_and_search/search.py
@@ -1,36 +1,58 @@
-class search:
-    def BinarySearch(Array, x):
-        lowerBound = 0
-        upperBound = len(Array)-1
-        index = -1
+from ..efficiency.functionTime import count_elapsed_time
 
-        while lowerBound < upperBound:
-            middlePoint = int((lowerBound+upperBound)/2)
-            if x==Array[middlePoint]:
-                index = middlePoint
-                break
-            else:
-                if x < Array[middlePoint]:
-                    upperBound = middlePoint-1
-                else:
-                    lowerBound = middlePoint+1
-        if lowerBound == upperBound and Array[lowerBound]==x:
-            index = lowerBound
-        return index
+@count_elapsed_time
+def linearsearch(self, list, target):
+    for i in range(0, len(list)):
+        if target == list[i]:
+            return i
+    return None
 
-    def BinarySearch_Recursive(Array, x, lB, uB):
-        middlePoint = int((lB+uB)/2)
+@count_elapsed_time
+def BinarySearch(self, Array, x):
+    lowerBound = 0
+    upperBound = len(Array)-1
+    index = -1
 
-        if lB == uB:
-            if x==Array[middlePoint]:
-                return middlePoint
-            else:
-                return -1
+    while lowerBound < upperBound:
+        middlePoint = int((lowerBound+upperBound)/2)
+        if x==Array[middlePoint]:
+            index = middlePoint
+            break
         else:
-            if x==Array[middlePoint]:
-                return middlePoint
+            if x < Array[middlePoint]:
+                upperBound = middlePoint-1
             else:
-                if x < Array[middlePoint]:
-                    return BinarySearch_Recursive(Array, x , lB, middlePoint)
-                else:
-                    return BinarySearch_Recursive(Array, x, middlePoint+1, uB)
+                lowerBound = middlePoint+1
+    if lowerBound == upperBound and Array[lowerBound]==x:
+        index = lowerBound
+    return index
+
+@count_elapsed_time
+def BinarySearch_Recursive(self, Array, x, lB, uB):
+    middlePoint = int((lB+uB)/2)
+
+    if lB == uB:
+        if x==Array[middlePoint]:
+            return middlePoint
+        else:
+            return -1
+    else:
+        if x==Array[middlePoint]:
+            return middlePoint
+        else:
+            if x < Array[middlePoint]:
+                return BinarySearch_Recursive(Array, x , lB, middlePoint)
+            else:
+                return BinarySearch_Recursive(Array, x, middlePoint+1, uB)
+
+@count_elapsed_time
+def interpolationSearch(key, array, left, right):
+    if right< left:
+        return - 1 # Not found
+    midpoint = left + (int) ((key - array[left]) / (array[right] - array[left]) * (right - left))
+    if key == array[midpoint]:
+        return midpoint # Found at position
+    if key > array[midpoint]:
+        return interpolationSearch(key, array, midpoint + 1, right) # Take the upper section
+    else:
+        return interpolationSearch(key, array, left, midpoint - 1) # Take the lower section
--- programs/sort_and_search/sort.py
+++ programs/sort_and_search/sort.py
@@ -1,50 +1,54 @@
-class sort:
-    
-    def bubble_sort(self, arr):
-        n = len(arr)
-        for i in range(0, n):
-            for j in range(0, n-i-1):
-                if arr[j+1] < arr[j]:
-                    arr[j+1], arr[j] = arr[j], arr[j+1]
-    	return arr
+from ..efficiency.functionTime import count_elapsed_time
 
-    def insertion(self, arr):
-        n = len(arr)
-        for i in range(1,n):
-            j = i
-            while j > 0 :
-                if arr[j-1] > arr[j] :
-                    arr[j-1], arr[j] = arr[j], arr[j-1]
-                    j -= 1
-                else:
-                    break
+@count_elapsed_time
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(0, n):
+        for j in range(0, n-i-1):
+            if arr[j+1] < arr[j]:
+                arr[j+1], arr[j] = arr[j], arr[j+1]
+	return arr
+
+@count_elapsed_time
+def insertion(arr):
+    n = len(arr)
+    for i in range(1,n):
+        j = i
+        while j > 0 :
+            if arr[j-1] > arr[j] :
+                arr[j-1], arr[j] = arr[j], arr[j-1]
+                j -= 1
+            else:
+                break
+    return arr
+
+@count_elapsed_time
+def select_sort(arr):
+    n = len(arr)
+    for i in range(0,n):
+        min = i
+        for j in range(i+1,n):
+            if arr[min] > arr[j]:
+                min = j
+        arr[i], arr[min] = arr[min], arr[i]
+    return arr
+
+@count_elapsed_time
+def Quick_Sort(arr):
+    less_subArr = []
+    greather_subArr = []
+    result = []
+    if len(arr)<=1:
         return arr
+    else:
+        pivot=arr[0]
+        for i in range(1,len(arr)):
+            if arr[i]<pivot:
+                less_subArr.append(arr[i])
+            else:
+                greather_subArr.append(arr[i])
 
-    def select_sort(self, arr):
-        n = len(arr)
-        for i in range(0,n):
-            min = i
-            for j in range(i+1,n):
-                if arr[min] > arr[j]:
-                    min = j
-            arr[i], arr[min] = arr[min], arr[i]
-        return arr
-
-    def Quick_Sort(self, arr):
-        less_subArr = []
-        greather_subArr = []
-        result = []
-        if len(arr)<=1:
-            return arr
-        else:
-            pivot=arr[0]
-            for i in range(1,len(arr)):
-                if arr[i]<pivot:
-                    less_subArr.append(arr[i])
-                else:
-                    greather_subArr.append(arr[i])
-
-            result.extend(Quick_Sort(less_subArr))
-            result.append(pivot)
-            result.extend(Quick_Sort(greather_subArr))
-            return result
+        result.extend(Quick_Sort(less_subArr))
+        result.append(pivot)
+        result.extend(Quick_Sort(greather_subArr))
+        return result
