ed604678446e1dc85df2ab0f0b1c81c0ad2596e7
Osmi Otalora Guerrero <ootalorague@uniminuto.edu.co>
2020-05-26 14:27:21 -0500

    arreglo a fractal mandelbrot

--- programs/fractals/Mandelbrot/fractal.py
+++ programs/fractals/Mandelbrot/fractal.py
@@ -1,10 +1,6 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# Created on Tue Apr 08 08:45:59 2014
-# License is MIT, see COPYING.txt for more details.
-# @author: Danilo de Jesus da Silva Bellini
+
 """
-Julia and Mandelbrot fractals image creation
+sMandelbrot fractals image creation
 """
 
 from __future__ import division, print_function
